cbcc6c5438682b5a994109c357c4f0d4262bd5b0
ProgramistaZpolski <piotrbadelek@protonmail.com>
2020-12-29 15:50:24 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,15 @@
+<?php
+class Router
+{
+    public function Route(String $url = "/", $html = "<h1>Hello World!</h1><i>Powered by Hotwire</i>")
+    {
+        if ($url == $_SERVER["REQUEST_URI"]) {
+            echo $html;
+        }
+    }
+}
+
+$flash = new Router();
+
+$flash->Route("/", "<h1>This is my homepage</h1>");
+$flash->Route("/about", "<adress>h@h.com</adress>");
