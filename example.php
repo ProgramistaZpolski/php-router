bad9272db2acf9e130b6fc375e17fe198e32880a
ProgramistaZpolski <piotrbadelek@protonmail.com>
2020-12-29 15:52:32 +0100

    Update example.php

--- example.php
+++ example.php
@@ -1,7 +1,7 @@
 <?php
 class Router
 {
-    public function Route(String $url = "/", $html = "<h1>Hello World!</h1><i>Powered by Hotwire</i>")
+    public function Route(String $url = "/", $html = "<h1>Hello World!</h1><i>Powered by php-router</i>")
     {
         if ($url == $_SERVER["REQUEST_URI"]) {
             echo $html;
